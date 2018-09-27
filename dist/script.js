ac774c6302d3e5a9e2b4d4021366c1d48b6efa91
Nuno Morais <nuno.mcvmorais@gmail.com>
2018-09-28 00:17:37 +0100

    second commit

---
+++ dist/script.js
@@ -0,0 +1,28 @@
+var string = "ola"
+!// HACK: hackerman!!!
+
+console.log(string)
+
+document.getElementById('id').style.color = "Benfica";
+
+if(fps < 60)
+  donwloadMoreWam("16g")
+  fps = 60;
+
+<!-- HACK INC -->
+
+
+try{
+
+
+
+
+
+
+
+} catch them all:
+
+
+
+
+if(failing) dont();
