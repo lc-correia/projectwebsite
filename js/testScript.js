2c49e38b015378036f642c2fd630fbd8ed53a6f6
HappytoGit <43241911+HappytoGit@users.noreply.github.com>
2018-09-28 18:18:03 +0100

    Added Assignments Folder

--- js/testScript.js
+++ js/testScript.js
@@ -1,3 +1,6 @@
-document.getElementById("testAssign").addEventListener("click", function(){
-    document.getElementById("testScript").style.color = "Red";
+var test = document.getElementById("testDiv");
+document.getElementById("testScript").addEventListener("click", function(){
+  console.log(test);
+    document.getElementById("testDiv").style.display = "block";
+    document.getElementById("mainImage").style.display = "none";
 });
