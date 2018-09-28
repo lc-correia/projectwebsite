bf9663ecbb51829ac46fb945563e0f5c2624b2a9
HappytoGit <43241911+HappytoGit@users.noreply.github.com>
2018-09-28 19:17:43 +0100

    added ass

--- js/testScript.js
+++ js/testScript.js
@@ -1,11 +1,11 @@
 document.getElementById("assign1Script").addEventListener("click", function(){
     document.getElementById("assign1").style.display = "block";
-        document.getElementById("assign2").style.display = "none";
+    document.getElementById("assign2").style.display = "none";
     //document.getElementById("mainImage").style.display = "none";
 });
 
 document.getElementById("assign2Script").addEventListener("click", function(){
     document.getElementById("assign2").style.display = "block";
-        document.getElementById("assign1").style.display = "none";
+    document.getElementById("assign1").style.display = "none";
     //document.getElementById("mainImage").style.display = "none";
 });
