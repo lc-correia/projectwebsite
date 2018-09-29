fd5532b1da57c709c095d7ca01a364a71a8f30c3
HappytoGit <43241911+HappytoGit@users.noreply.github.com>
2018-09-29 19:00:26 +0100

    removed shit

--- js/testScript.js
+++ js/testScript.js
@@ -2,14 +2,12 @@ document.getElementById("assign1Script").addEventListener("click", function(){
     document.getElementById("assign1").style.display = "block";
     document.getElementById("assign2").style.display = "none";
     document.getElementById("assign0").style.display = "none";
-    //document.getElementById("mainImage").style.display = "none";
 });
 
 document.getElementById("assign2Script").addEventListener("click", function(){
     document.getElementById("assign2").style.display = "block";
     document.getElementById("assign1").style.display = "none";
     document.getElementById("assign0").style.display = "none";
-    //document.getElementById("mainImage").style.display = "none";
 });
 
 window.addEventListener("load", function(){
