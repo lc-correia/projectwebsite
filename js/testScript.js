9aae6a8cd2d08bdf79d0b15496b12e0004ea3ca6
Nuno Morais <nuno.mcvmorais@gmail.com>
2018-09-29 19:47:42 +0100

    MAKE IT LIVE

--- js/testScript.js
+++ js/testScript.js
@@ -15,12 +15,15 @@ window.addEventListener("load", function(){
 
   var day = date.getDate();
   var month = date.getMonth();
+  var year = date.getFullYear();
 
-  if(day >= 1 && (month + 1) >= 10) {
+  if(day >= 1 && (month + 1) >= 10 && year >= 2018) {
+    document.getElementById("assign0RightTab").style.display = "none";
     document.getElementById('assign1RightTab').style.display = "block";
   }
 
-  if(day >= 8 && (month + 1) >= 10) {
+  if(day >= 8 && (month + 1) >= 10 && year >= 2018) {
     document.getElementById('assign2RightTab').style.display = "block";
   }
+
 });
