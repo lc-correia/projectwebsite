100fe9e6102db5477acda72fe5019a321849ec2b
HappytoGit <43241911+HappytoGit@users.noreply.github.com>
2018-10-08 11:40:35 +0100

    Added scroll to "About Us Div" from our contacts (when clicked on our names)

--- js/index.js
+++ js/index.js
@@ -1,5 +1,24 @@
 $('#about-us-btn').on('click', function() {
     $.smoothScroll({scrollTarget: '#about_us'});
   return false;
-  
+});
+
+$('#contact1').on('click', function() {
+    $.smoothScroll({scrollTarget: '#about_us'});
+  return false;
+});
+
+$('#contact2').on('click', function() {
+    $.smoothScroll({scrollTarget: '#about_us'});
+  return false;
+});
+
+$('#contact3').on('click', function() {
+    $.smoothScroll({scrollTarget: '#about_us'});
+  return false;
+});
+
+$('#contact4').on('click', function() {
+    $.smoothScroll({scrollTarget: '#about_us'});
+  return false;
 });
